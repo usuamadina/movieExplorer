280d1986c8e0d172507d9f79a05519084d48d546
umadina <umadina@euro-help.es>
2017-05-02 15:15:54 +0200

    refactoring

--- www/js/app/films/OMDBFilmsFactory.js
+++ www/js/app/films/OMDBFilmsFactory.js
@@ -1,9 +1,8 @@
 (function() {
 
-
     angular
 
-    .module('OMDBFilmsFactory', ['FilmModel', 'Constants'])
+        .module('OMDBFilmsFactory', ['FilmModel', 'Constants'])
 
     .factory('FilmsFactory', FilmsFactory);
 
@@ -15,8 +14,17 @@
         filmsFactory.films = [];
         filmsFactory.selectedFilm = null;
 
+        var service = {
+            urlFromTitle: urlFromTitle,
+            selectedFilmByTitle: selectedFilmByTitle,
+            getFilm: getFilm,
+            getFilms: getFilms
 
-        var urlFromTitle = function(title) {
+        };
+        return service;
+
+
+        function urlFromTitle(title) {
             //replace Spaces by +
             var queryString = title.split(' ').join('+');
             //replace placeholder with query        
@@ -25,7 +33,7 @@
         }
 
 
-        var selectedFilmByTitle = function(title) {
+        function selectedFilmByTitle(title) {
 
             for (var i = 0; i < filmsFactory.films.length; i++) {
                 if (filmsFactory.films[i].title === title) {
@@ -37,15 +45,14 @@
         }
 
 
-        filmsFactory.getFilm = function(title) {
+        function getFilm(title) {
 
-            var deferred = $q.defer();           
-            if (filmsFactory.films.length > 0) {                
+            var deferred = $q.defer();
+            if (filmsFactory.films.length > 0) {
                 filmsFactory.selectedFilm = selectedFilmByTitle(title);
                 //el resolve llama al callback de response (he encontrado el dato y te lo paso)
                 deferred.resolve(filmsFactory.selectedFilm);
             } else {
-                  console.log("filmsFactory.getFilm");
 
                 //$http es un servicio asíncrono, usaremos promesas
                 //para recoger los datos de las peticiones
@@ -66,7 +73,7 @@
         }
 
 
-       filmsFactory.getFilms = function() {
+        function getFilms(){
             var deferred = $q.defer();
             console.log(filmsFactory.films.length);
             if (filmsFactory.films.length > 0) {
@@ -113,111 +120,7 @@
             return deferred.promise;
         }
 
-        return filmsFactory;
+      // return filmsFactory;
     }
 
 })();
-
-
-/* FilmFactory.$inject('$http', '$q', 'filmNames', 'omdbApi', 'Film');
-     function FilmsFactory($http, $q, filmNames, omdbApi, Film) {
-
-        var filmsFactory = {};
-        filmsFactory.films = [];
-        filmsFactory.selectedFilm = null;
-
-        var urlFromTitle = function(title) {
-            //replace Spaces by +
-            var queryString = title.split(' ').join('+');
-            //replace placeholder with query        
-            var url = omdbApi.url.replace(omdbApi.namePlaceholder, queryString);
-            return url;
-        }
-
-
-        var selectedFilmByTitle = function(title) {
-
-            for (var i = 0; i < filmsFactory.films.length; i++) {
-                if (filmsFactory.films[i].title === title) {
-                    return filmsFactory.films[i];
-                    //return films.film;
-                }
-            };
-            return null;
-        }
-
-
-        filmsFactory.getFilm = function(title) {
-
-            var deferred = $q.defer();
-            if (filmsFactory.films.length > 0) {
-                filmsFactory.selectedFilm = selectedFilmByTitle(title);
-                //el resolve llama al callback de response (he encontrado el dato y te lo paso)
-                deferred.resolve(filmsFactory.selectedFilm);
-            } else {
-                //$http es un servicio asíncrono, usaremos promesas
-                //para recoger los datos de las peticiones
-
-                $http.get(urlFromTitle(title), {}).then(
-                    function(response) {
-                        filmsFactory.selectedFilm = Film.build(response.data);
-                        deferred.resolve(filmsFactory.selectedFilm);
-                    },
-
-                    function(error) {
-                        filmsFactory.selectedFilm = null;
-                        deferred.resolve(null);
-                    });
-            }
-            return deferred.promise;
-
-        }
-
-
-        filmsFactory.getFilms = function() {
-
-            var deferred = $q.defer();
-            if (filmsFactory.films.length > 0) {
-                deferred.resolve(filmsFactory.films);
-            } else {
-                var nDownloads = 0;
-                var someErrorOccured = false;
-                var resolveIfFinished = function(success) {
-                    nDownloads++;
-                    if (!success) {
-                        someErrorOccured = true;
-                    }
-
-                    if (nDownloads === filmNames.length) {
-                        if (!someErrorOccured) {
-                            console.log("ha ocurrido un error");
-                            deferred.resolve(filmsFactory.films);
-                        } else {
-                            console.log("ha ido todo bien");
-                            deferred.reject();
-                        }
-                    }
-                };
-
-
-                for (var i = 0; i < filmNames.length; i++) {
-                    $http.get(urlFromTitle(filmNames[i]), {}).then(
-                        function(response) {
-                            filmsFactory.films.push(Film.build(response.data));
-                            resolveIfFinished(true);
-
-                        },
-                        function(error) {
-                            resolveIfFinished(false);
-                        }
-
-                    );
-
-                }
-            }
-            return deferred.promise;
-        };
-
-        return filmsFactory;
-
-    }*/
